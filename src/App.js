c23182277e624b23adb871a41dec66a11c463f0a
Eugen-Alexander Habrony <eugen-alexander.habrony@adesso-mobile.de>
2022-01-13 01:43:31 +0100

    Adding Form Inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+
+import {NewExpense} from './components/NewExpense/NewExpense' ;
 import Expenses from './components/Expenses/Expenses';
+
 
 const App = () => {
 	const expenses = [
@@ -26,7 +29,7 @@ const App = () => {
 
 	return (
 		<div className="App">
-			<h1>Let's get started!</h1>
+			<NewExpense/>
 			<Expenses items={expenses}/>
 		</div>
 	);
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+export const NewExpense = () => {
+	return (
+		<div className="new-expense">
+			<ExpenseForm/>
+		</div>
+	);
+}
